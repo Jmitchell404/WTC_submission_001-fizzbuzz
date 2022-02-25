6c15e257c9fe8e86078fa1d67c5ad6c443da4b39
jmitchel020 <jmitchel020@student.wethinkcode.co.za>
2022-02-25 16:50:49 +0200

    FizzBuzz

--- src/test/java/za/co/wethinkcode/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/za/co/wethinkcode/fizzbuzz/FizzBuzzTest.java
@@ -15,16 +15,36 @@ public class FizzBuzzTest {
 
     @Test
     public void divisibleBy3() {
-        // Insert your test code here
+
     }
+        public void generateUpTo3() {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            String result = fizzBuzz.countTo(15);
+            assertEquals(
+                    "Fizz",
+                    result);
+        }
 
     @Test
     public void divisibleBy5() {
-        // Insert your test code here
+    }
+    public void generateUpTo5() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.countTo(15);
+        assertEquals(
+                "Buzz",
+                result);
     }
 
-    @Test void divisibleBy3And5() {
-        // Insert your test code here
+    @Test 
+    public void divisibleBy3And5() {
+    }
+        public void generateUpTo3and5() {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            String result = fizzBuzz.countTo(15);
+            assertEquals(
+                    "Fizz",
+                    result);
     }
 
     @Test
